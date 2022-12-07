a84e144f5fa49ce3dcc074e23d8e173f12ca40e9
CharlesGodinho <charleshenriquesouza@gmail.com>
2022-12-07 07:51:53 -0300

    new folders

--- src/main/java/com/charles/demo/repositories/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package com.charles.demo.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import com.charles.demo.models.Book;
-
-@Repository
-public interface BookRepository extends JpaRepository<Book, Long> {}
